b909b1289b7f81c3de5ab007ba56ab82d5b7e6b9
Aakash Shah <aakash.shah@publicissapient.com>
2019-09-10 10:03:39 +0530

    added prop-types for React-project

--- packages/wpComponentStructure/src/components/aem/Foo/index.js
+++ packages/wpComponentStructure/src/components/aem/Foo/index.js
@@ -1,41 +1,46 @@
-import $ from 'jquery'
+import $ from 'jquery';
+
 import React, {
-  Component
-} from 'react'
-import ReactDOM from 'react-dom'
+  Component,
+} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 export default class FooButton extends Component {
-  constructor(human){
+  constructor(human) {
     super(human);
   }
 
   handleClick() {
     try {
-      const { buttonTitle } = this.props.content
-      alert(`Yay! It's ${buttonTitle}..`)
+      const { buttonTitle } = this.props.content;
+      alert(`Yay! It's ${buttonTitle}..`);
     } catch (e) {
       console.error(e);
     }
   }
 
   render() {
-    return(
+    return (
       <div>
         <button type = "button" onClick={this.handleClick.bind(this)}>
           { this.props.content.buttonTitle + 'hello...'}
         </button>
       </div>
-    )
+    );
   }
 }
 
-$('.foo-button-component').each(iterate_FooButtonComponents);
+FooButton.propTypes = {
+  content: PropTypes.object
+};
 
+$('.foo-button-component').each(iterate_FooButtonComponents);
 
 function iterate_FooButtonComponents() {
   let props=$(this).attr('data-props');
   props = JSON.parse(props);
   ReactDOM.render(
-    <FooButton content={props}/>, this
+      <FooButton content={props}/>, this
   );
 }
